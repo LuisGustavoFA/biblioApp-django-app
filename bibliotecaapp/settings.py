190e3e19a8c908437ad21b5596e0b4255952fce2
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2025-12-17 17:26:56 -0300

    fix: remove encoding not present in thin mode

--- bibliotecaapp/settings.py
+++ bibliotecaapp/settings.py
@@ -103,10 +103,6 @@ DATABASES = {
          'NAME': config('DB_NAME'),
          'USER': config('DB_USER'),
          'PASSWORD': config('DB_PASSWORD'),   
-         'OPTIONS': {
-            'encoding': 'UTF-8',
-            'timeout': 10 
-        }
     }
 }
 
